331810c3bfff4fd4762f3a1b589de18ce80fa8a4
TTolsun <baboess@gmail.com>
2023-06-01 15:47:38 +0900

    [feature] 37p
    Signed-off-by: TTolsun <baboess@gmail.com>

--- PrimeFactorTest/prime-factors_test.cpp
+++ PrimeFactorTest/prime-factors_test.cpp
@@ -3,9 +3,16 @@
 
 using namespace std;
 
-TEST(TestCaseName, TestName)
+TEST(TestCaseName, Of1)
 {
 	PrimeFactor primeFactor{};
 	const vector<int> expected{};
 	EXPECT_EQ(primeFactor.of(1), expected);
+}
+
+TEST(TestCaseName, Of2)
+{
+	PrimeFactor primeFactor{};
+	const vector<int> expected{2};
+	EXPECT_EQ(primeFactor.of(2), expected);
 }
